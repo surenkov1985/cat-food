3ba62f61b7aba34850199b3921516b78bea3dd4b
surenkov1985 <surenkov1985@gmail.com>
2022-07-06 13:29:57 +0300

    добавлены стили выбранного и неактивного товара, изменение текста в карточках товара при выборе карточки,  сделана проверка на валидаторе

--- src/assets/components/App.js
+++ src/assets/components/App.js
@@ -1,18 +1,86 @@
-import React from "react"
+import React, {useState, useRef, useEffect} from "react"
 import ProductBlock from "./ProductBlock"
 
 export default function App() {
 
+	const fugRef = useRef();
+	const fishRef = useRef();
+	const duckRef = useRef();
+
+	const [fugText, setFugText] = useState();
+	const [fishText, setFishText] = useState();
+	const [duckText, setDuckText] = useState();
+
+	useEffect(() => {
+		setBlockText(fugRef);
+		setBlockText(fishRef);
+		setBlockText(duckRef);
+	}, []);
+
+	function setHover(ref) {
+
+		if (ref.current.classList.contains("active")) {
+
+			ref.current.classList.add("hover")
+		}
+	}
+
+	function setBlockText(ref) {
+
+		if (ref === fugRef && ref.current.classList.contains("disabled")) {
+
+			setFugText("Печалька, с фуа-гра закончился.");
+		} else if (ref === fugRef && !ref.current.classList.contains("active")) {
+
+			setFugText("Чего сидишь? Порадуй котэ,");
+		} else if (ref === fugRef && ref.current.classList.contains("active")) {
+
+			setFugText("Печень утки разварная с артишоками.");
+		}
+
+		if (ref === fishRef && ref.current.classList.contains("disabled")) {
+
+			setFishText("Печалька, с рыбой закончился.");
+		} else if (ref === fishRef && !ref.current.classList.contains("active")) {
+
+			setFishText("Чего сидишь? Порадуй котэ,");
+		} else if (ref === fishRef && ref.current.classList.contains("active")) {
+
+			setFishText("Головы щучьи с чесноком да свежайшая сёмгушка.");
+		}
+
+		if (ref === duckRef && ref.current.classList.contains("disabled")) {
+
+			setDuckText("Печалька, с курой закончился.");
+		} else if (ref === duckRef && !ref.current.classList.contains("active")) {
+
+			setDuckText("Чего сидишь? Порадуй котэ,");
+		} else if (ref === duckRef && ref.current.classList.contains("active")) {
+
+			setDuckText("Филе из цыплят с трюфелями в бульоне.");
+		}
+	}
+
+	function cardHandler(ref) {
+
+		if (!ref.current.classList.contains("disabled")) {
+
+			ref.current.classList.toggle("active")
+		}
+
+		setBlockText(ref);
+	}
+
 	return (
 		<>
-			<div className="background"></div>
+			<div className="background"/>
 			<div className="title">
 				<h1>Ты сегодня покормил кота?</h1>
 			</div>
 			<div className="container">
-				<ProductBlock/>
-				<ProductBlock/>
-				<ProductBlock/>
+				<ProductBlock className={"container__block block"} ref={fugRef} text={fugText} comp="с фуа-гра" numb="10" present="мыши" weight="0.5" onHand={() => cardHandler(fugRef)} onHover={setHover}/>
+				<ProductBlock className={"container__block block"} ref={fishRef} text={fishText} comp="с рыбой" numb="40" present="2 мыши" weight="2" onHand={() => cardHandler(fishRef)} onHover={setHover}/>
+				<ProductBlock className={"container__block block disabled"} ref={duckRef} text={duckText} comp="с курой" numb="100" present="5 мышей" subpresent="заказчик доволен" weight="5" onHand={() => cardHandler(duckRef)} onHover={setHover}/>
 			</div>
 		</>
 	)
--- src/assets/components/ProductBlock.js
+++ src/assets/components/ProductBlock.js
@@ -1,27 +1,30 @@
-import React from "react"
+import React, {forwardRef} from "react"
 
-export default function ProductBlock() {
+const ProductBlock = forwardRef((props, ref) => {
 
 	return (
-		<div className="container__block block">
-			<article className="block__card card">
+		<div ref={ref} className={props.className}>
+			<article className="block__card card" style={{backgroundColor: props.color, borderColor: props.color}} onClick={props.onHand} onMouseOut={() => {props.onHover(ref)}}>
 				<div className="card__description">
-					<strong className="card__description_text">Сказочное заморское яство</strong>
+					<strong className="card__description_text" style={{color: props.color}}>Сказочное заморское яство</strong>
 					<h2 className="card__title">Нямушка</h2>
-					<h3 className="card__subtitle">с фуа-гра</h3>
-					<p className="card__numb">10 порций</p>
-					<p className="card__present">мышь в подарок</p>
+					<h3 className="card__subtitle">{props.comp}</h3>
+					<p className="card__numb">{props.numb} порций</p>
+					<p className="card__present">{props.present} в подарок</p>
+					<p className="card__subpresent">{props.subpresent}</p>
 				</div>
-				<div className="card__cat"></div>
-				<div className="card__width">
-					<div className="card__width_numb">0,5</div>
+				<div className="card__cat"/>
+				<div className="card__width" style={{backgroundColor: props.color}}>
+					<div className="card__width_numb">{props.weight}</div>
 					<div className="card__width_text">кг</div>
 				</div>
 			</article>
 			<div className="block__text">
-				<p>Чего сидишь? Порадуй котэ, </p>
-				<button className="block__button">купи</button>
+				<p>{props.text}</p>
+				<button className="block__button" onClick={props.onHand}>купи</button>
 			</div>
 		</div>
 	)
-}
+});
+
+export default ProductBlock;
